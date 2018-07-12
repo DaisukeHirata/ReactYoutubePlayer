8b7fe5530507e131eee9f426a785832329d5ec8a
Daisuke Hirata <daisukihirata@gmail.com>
2018-07-12 23:52:49 +0900

    search youtube

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import SearchBar from './search_bar';
+import YTSearch from 'youtube-api-search';
+
+const API_KEY = 'AIzaSyA-EGk741oZgzkwAQF-i9z91IGO8gEZ5DA';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
